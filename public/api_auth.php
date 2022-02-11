3e0dfea08e3091ef4c66342fba1b3fcb0dbacad6
filip-kor <99483104+filip-kor@users.noreply.github.com>
2022-02-11 15:44:50 +0000

    Create api_auth.php

---
+++ public/api_auth.php
@@ -0,0 +1,9 @@
+<?php
+
+$session = new SpotifyWebAPI\Session(
+    'd1dd21d43953476b9968b0e503c7006f', // client id
+    'c3257cc13275469a8dcde398e25297b0', // secret
+    'http://localhost/'
+);
+
+?>
